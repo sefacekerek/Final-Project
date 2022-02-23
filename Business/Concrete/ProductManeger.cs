f62c6a7955d139765926689dc472ce3772ba03af
sefacekerek <sefacekerek2014.pa@gmail.com>
2022-02-23 12:18:02 +0100

    Project added

---
+++ Business/Concrete/ProductManeger.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManeger : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManeger(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
+    
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManeger productManeger = new ProductManeger( new InMemoryProductDal());
+            foreach (var product in productManeger.GetAll() )
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+        }
+    }
+}
